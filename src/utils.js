78c6a936d3b97a4c2bcd061669e37563afb222fc
evry1lovesjay <evry1lovesjay@yahoo.com>
2023-05-11 14:25:10 +0100

    added render url

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,13 @@
 import  axios from "axios";
 
-export const axiosInstance = axios.create({
-    baseURL: "http://localhost:6500/api",
-    withCredentials: true,
-})
-
 // export const axiosInstance = axios.create({
-//     baseURL: "https://todo-v3-server.onrender.com/api",
+//     baseURL: "http://localhost:6500/api",
 //     withCredentials: true,
 // })
+
+export const axiosInstance = axios.create({
+    baseURL: "https://booking-app-api-iiun.onrender.com/api",
+    withCredentials: true,
+})
 
 // "proxy":"http://localhost:6500/api"
