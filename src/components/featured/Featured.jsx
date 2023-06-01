33c13d78430fbc364c28132de5d426f92f4a3115
evry1lovesjay <evry1lovesjay@yahoo.com>
2023-06-01 12:33:43 +0200

    added prod & dev

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -17,7 +17,7 @@ const {data, loading, error} = useFetch("/hotels/countByCity?cities=berlin,londo
                     </div>
                 </div>
                 <div className="featuredItem">
-                    <img src="https://cf.bstatic.com/xdata/images/city/600x600/613095.jpg?k=8caf960d96a59e284ac1518ac8777e89d17fda6572acd84dbec151f627c7bf07&o=" alt="" className="featuredImg" />
+                    <img src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o=" alt="" className="featuredImg" />
                     <div className="featuredTitles">
                         <h1>London</h1>
                         <h1>{data[1]} properties</h1>
--- src/utils.js
+++ src/utils.js
@@ -5,9 +5,20 @@ import  axios from "axios";
 //     withCredentials: true,
 // })
 
-export const axiosInstance = axios.create({
-    baseURL: "https://booking-app-api-iiun.onrender.com/api",
-    withCredentials: true,
-})
 
+// export const axiosInstance = axios.create({
+    //     baseURL: "https://booking-app-api-iiun.onrender.com/api",
+    //     withCredentials: true,
+    // })
+    
 // "proxy":"http://localhost:6500/api"
+
+    
+const API_URL = process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_API_URL_PROD
+    : process.env.REACT_APP_API_URL_DEV;
+    
+    export const axiosInstance = axios.create({
+        baseURL: `${API_URL}/api` ,
+        withCredentials: true,  
+    })
